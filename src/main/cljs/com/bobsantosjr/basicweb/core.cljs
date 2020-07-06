044e4bab454b74dc74c60871b4f83e2ca607f9b6
bobsantos <bob.santosjr@gmail.com>
2020-07-06 22:52:16 +0300

    moved cljs deps to deps.edn and added secretary for routing

--- src/main/cljs/com/bobsantosjr/basicweb/core.cljs
+++ src/main/cljs/com/bobsantosjr/basicweb/core.cljs
@@ -1,4 +1,28 @@
-(ns com.bobsantosjr.basicweb.core)
+(ns com.bobsantosjr.basicweb.core
+  (:require [secretary.core :as secretary]
+            [goog.events :as events])
+  (:require-macros [secretary.core :refer [defroute]])
+  (:import [goog History]
+           [goog.history EventType]))
+
+(def app
+  (js/document.getElementById "app"))
+
+(defn set-html! [el content]
+  (aset el "innerHTML" content))
+
+(secretary/set-config! :prefix "#")
+
+(defroute home-path "/" []
+          (set-html! app "<h1>Home</h1>"))
+
+(defroute "*" []
+          (set-html! app "<h1>Page Not Found</h1>"))
+
+(let [h (History.)]
+  (events/listen h EventType.NAVIGATE #(secretary/dispatch! (.-token %)))
+  (doto h
+    (.setEnabled true)))
 
 (defn init! []
-  (println "ClojureScript app running"))
+  (secretary/dispatch! "/"))
